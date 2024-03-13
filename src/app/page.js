d41b9b863f07cedcf495239e22923dbbed4f0543
MickMacha <kimmike1100@gmail.com>
2024-03-13 14:12:44 +0300

    added skills to portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,21 @@
 import Head from "next/head"
 import {BsMoonStarsFill} from "react-icons/bs"
 import {AiFillGithub, AiFillTwitterCircle, AiFillLinkedin} from "react-icons/ai"
+import { SiHtml5 } from "react-icons/si";
+import { DiDjango } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaBootstrap } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiFastapi } from "react-icons/si";
+import { SiFlask } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { SiPytorch } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+
 
 export default function Home() {
   return (
@@ -24,7 +39,7 @@ export default function Home() {
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-700 font-medium">Michael Macharia</h2>
             <h3 className="text-2xl py-2">Software Developer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">Hi, I'm Mike. I'm a software developer based in Nairobi, Kenya. I have a passion for software development and love to fullstack applications.</p>
+            <p className="text-md py-5 leading-8 text-gray-800">Hi, I'm Mike. I'm a software developer based in Nairobi, Kenya. I have a passion for software development and Ai/Machine Learning.</p>
             
           </div>
           <div>
@@ -32,6 +47,75 @@ export default function Home() {
               <AiFillTwitterCircle className="text-2xl cursor-pointer"/>
               <AiFillLinkedin className="text-2xl cursor-pointer"/>
             </div>
+            <div className="flex flex-col justify-center text-center">
+              <div className="text-center p-10 py-10">
+              <h2 className="text-5xl py-2 text-teal-700 ml-15 font-medium">Skills</h2>
+              <p className="text-md py-5 leading-8 text-gray-800">I have experience in the following technologies:</p>
+              </div>
+              {/* generate icons for bootsrrap css react extjs tailwind nodejs django flask and fast api for react */}
+
+              <div className=" flex justify-center ">
+                <div className=" max-w-sm p-5 items-center">
+                  <SiHtml5 className="text-5xl text-red-500"/>
+                  <p>HTML5</p>
+                </div>
+                <div className="flex flex-col p-5 items-center">
+                  <FaReact className="text-5xl text-red-500"/>
+                  <p>React</p>
+                </div>
+                <div className= "flex flex-col p-5 items-center">
+                  <DiDjango className="text-5xl text-teal-600"/>
+                  <p>Django</p>
+                </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <TbBrandNextjs className="text-5xl text-teal-600"/>
+                  <p>Nextjs</p>
+                </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <FaBootstrap className="text-5xl text-purple-600"/>
+                  <p>Bootstrap</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <FaCss3Alt className="text-5xl text-blue-600"/>
+                  <p>CSS3</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <SiFastapi className="text-5xl text-green-600"/>
+                  <p>Fastapi</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <SiFlask className="text-5xl text-green-600"/>
+                  <p>Flask</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <SiMysql className="text-5xl text-blue-600"/>
+                  <p>Mysql</p>
+                  </div>
+
+                  <div className=" flex flex-col p-5 items-center">
+                  <SiMongodb className="text-5xl text-green-600"/>
+                  <p>Mongodb</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <SiPytorch className="text-5xl text-red-600"/>
+                  <p>Pytorch</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <FaNodeJs className="text-5xl text-green-600"/>
+                  <p>Nodejs</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <SiJavascript className="text-5xl text-yellow-600"/>
+                  <p>Javascript</p>
+                  </div>
+                <div className=" flex flex-col p-5 items-center">
+                  <SiTailwindcss className="text-5xl text-blue-600"/>
+                  <p>Tailwindcss</p>
+                  </div>
+
+        
+                </div>
+              </div>
         </section>
       </main>
     </body>
